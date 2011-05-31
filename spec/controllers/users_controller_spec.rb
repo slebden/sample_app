d6e905968955261f41871969a0c10139e955f202
slebden <slebden@gmail.com>
2011-05-31 10:52:43 +0200

    Basic user model complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,21 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
+
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -12,6 +27,23 @@ describe UsersController do
     it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
+    end
+  end
+
+  describe "GET 'show'" do
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
     end
   end
 
