d46c89b277e9ed69b8f463c135c511b775860730
slebden <slebden@gmail.com>
2011-05-31 10:05:23 +0200

    Added salt for passwords & password functions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,6 +95,24 @@ describe User do
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
   end
 
+  describe "has_password? method" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should be true if the passwords match" do
+      @user.has_password?(@attr[:password]).should be_true
+    end    
+
+    it "should be false if the passwords don't match" do
+      @user.has_password?("invalid").should be_false
+    end 
+  end
 end
